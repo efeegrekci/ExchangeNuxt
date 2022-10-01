3e43f7c081213f181f8591259fda76226452bc79
efeegrekci <efe.egrekci@gmail.com>
2022-10-01 03:28:33 +0300

    first commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "exchangenuxt",
+    title: "ExchangeNuxt",
     htmlAttrs: {
       lang: "en",
     },
